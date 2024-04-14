1cd9318d46b18315bacb8733ef8719b72a3af437
ssamio <sami.o.seppala@proton.me>
2024-04-15 00:02:30 +0300

    Initial commit, again

---
+++ Exercises/dist/js/main.js
@@ -0,0 +1,23 @@
+// Select DOM items
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+//Set initial state of the menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  menuBtn.classList.toggle("close");
+  menu.classList.toggle("show");
+  menuNav.classList.toggle("show");
+  menuBranding.classList.toggle("show");
+  navItems.forEach((item) => item.classList.toggle("show"));
+
+  //Set menu state
+  showMenu = !showMenu;
+}
